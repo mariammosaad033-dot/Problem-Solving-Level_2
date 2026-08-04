c810d93cf0d02ea008c3b19827ad2112b36c1f8a
mariam mosad <mariammosaad121@gmail.com>
2026-08-04 06:22:31 +0300

    Add project files.

---
+++ problem 1 level 2.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+using namespace std;
+void PrintTableHeader()
+{
+	cout << "\n\n\t\t\tMultiplication Table From 1 To 10 \n\n";
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << "\t" << i;
+	}
+	cout << "\n---------------------------------------------------------------------------------------------\n";
+}
+string ColumSperator(int i)
+{
+	if (i < 10)
+		return "   |";
+	else
+		return "  |";
+}
+void PrintMultiplicationTable()
+{
+	PrintTableHeader();
+	for (int i = 1;i <= 10;i++)
+	{
+		cout << " " << i << ColumSperator(i);
+		for (int j = 1;j <= 10;j++)
+		{
+			cout << "\t"<< j * i ;
+		}
+		cout << endl;
+	}
+}
+int main()
+{
+	PrintMultiplicationTable();
+	return 0;
+}
+
