b3eba3597067f28c8eb631546ab2cb1d39c12ccc
maryam mossad <mariammosaad033@gmail.com>
2026-08-14 06:32:07 +0300

    Create FillArrayWithRandomNumbersFrom1To100.cpp

---
+++ FillArrayWithRandomNumbersFrom1To100.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+    cout << "Please enter the number of element " << endl;
+    cin >> arrLength;
+
+    for (int i = 0;i < arrLength;i++)
+    {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0;i < arrLength;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength;
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+    cout << "Array Element : ";
+    PrintArray(arr, arrLength);
+
+    return 0;
+}
