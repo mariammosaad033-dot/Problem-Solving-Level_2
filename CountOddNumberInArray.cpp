ad1535ebaeb3861eb2273687bf919e156b5659c8
maryam mossad <mariammosaad033@gmail.com>
2026-08-14 20:37:58 +0300

    Create CountOddNumberInArray.cpp

---
+++ CountOddNumberInArray.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+int Randomnumber(int from, int to)
+{
+    int randum = rand() % (to - from + 1) + from;
+    return randum;
+}
+
+void FillArray(int arr[100], int& arrlength)
+{
+    cout << "please enter the number of element  "<<endl;
+    cin >> arrlength;
+
+    for (int i = 0;i < arrlength;i++)
+    {
+        arr[i] = Randomnumber(1, 100);
+    }
+}
+void printArray(int arr[100], int arrlength)
+{
+    for (int i = 0;i < arrlength;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int OddCount(int arr[100], int arrlength)
+{
+    int counter = 0;
+
+    for (int i = 0;i < arrlength;i++)
+    {
+        if (arr[i] % 2 == 1)
+        {
+            counter++;
+        }
+            
+    }
+    return counter;
+}
+int main()
+{
+    int arr[100], arrlength;
+
+    FillArray(arr, arrlength);
+    cout << "Array 1 element ";
+    printArray(arr, arrlength);
+
+    cout << "odd number count is :  ";
+    cout<< OddCount(arr, arrlength);
+
+    return 0;
+}
